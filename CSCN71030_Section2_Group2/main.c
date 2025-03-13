fa356500e3b7a3ee96e5ecc32b9f8b4f08671a33
SanctumSpear <146847788+SanctumSpear@users.noreply.github.com>
2025-03-13 16:05:47 -0400

    Finish basic main game loop with player functions

--- CSCN71030_Section2_Group2/main.c
+++ CSCN71030_Section2_Group2/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
 #include "Card.h"
 #include "Deck.h"
-#include <string.h>
+#include "Player.h"
+#include "Dealer.h"
 
 int main(void) {
 	//Test code for card and deck stuff
@@ -16,11 +20,32 @@ int main(void) {
 	//printf_s("\n\n");
 
 	//printDeck(testDeck);
-	char begin[64] = "0";
+	char buffer[64] = "0";
 	char start[] = "Start";
 	do {
 		printf("Welcome to fortnite peter. To begin, type 'Start' \n");
-	} while (scanf_s("%63s", begin, (unsigned int)sizeof(begin)) != 1 || strcmp(start, begin) != 0);
-	printf("%s", begin);
+	} while (scanf_s("%63s", buffer, (unsigned int)sizeof(buffer)) != 1 || strcmp(start, buffer) != 0);
 	
+	StartPlayerTurn();
+	
+	char choice = 'n';
+	bool playerTurnActive = true;
+
+	printf("This is epic blackjack, type 'h' to Hit or type 's' to Stand \n");
+	while (playerTurnActive && scanf_s(" %c", &choice, (unsigned int)sizeof(choice))) {
+		printf("The dealer has: \n"); //TODO: print the dealer's hand (each card) and the total value, but only the first card
+		printf("You have: \n"); //TODO: print the player's hand (each card) and the total value	
+
+		switch (choice) {
+		case 'h':
+			PlayerHit();
+			break;
+		case 's':
+			EndPlayerTurn();
+			playerTurnActive = false;
+			break;
+		default:
+			printf("Invalid choice\n");
+		}
+	}
 }
