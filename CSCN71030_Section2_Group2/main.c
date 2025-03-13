2b42f94fc35b8a7477056261a59c1dc6b341f297
SanctumSpear <146847788+SanctumSpear@users.noreply.github.com>
2025-03-13 11:13:26 -0400

    Start working on main input loop

--- CSCN71030_Section2_Group2/main.c
+++ CSCN71030_Section2_Group2/main.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include "Card.h"
 #include "Deck.h"
+#include <string.h>
 
 int main(void) {
 	//Test code for card and deck stuff
 
-	CARD testCard = createCard(1, 1);
-	printCard(testCard);
-	printf_s("\n\n");
+	//CARD testCard = createCard(1, 1);
+	//printCard(testCard);
+	//printf_s("\n\n");
 
-	DECK testDeck = createDeck();
-	CARD deckCard = drawCard(&testDeck);
-	printCard(deckCard);
-	printf_s("\n\n");
+	//DECK testDeck = createDeck();
+	//CARD deckCard = drawCard(&testDeck);
+	//printCard(deckCard);
+	//printf_s("\n\n");
 
-	printDeck(testDeck);
+	//printDeck(testDeck);
+	char begin[64] = "0";
+	char start[] = "Start";
+	do {
+		printf("Welcome to fortnite peter. To begin, type 'Start' \n");
+	} while ( scanf_s("%63s", begin, (unsigned int)sizeof(begin) ) != 1 || !strncmp(start, begin,6) );
+	printf("%s", begin);
 	
 }
