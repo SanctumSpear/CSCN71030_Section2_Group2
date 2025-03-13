41c5c7cc6a372ee3200f16f07d8c4920ac6d83ba
SanctumSpear <146847788+SanctumSpear@users.noreply.github.com>
2025-03-13 10:25:55 -0400

    Remove parameters for hand in hand functions

--- CSCN71030_Section2_Group2/Hand.c
+++ CSCN71030_Section2_Group2/Hand.c
@@ -1,13 +1,13 @@
 #include "Hand.h"
 
-void deal(DECK* deck, CARD hand[HAND_ARRAY_LENGTH][HAND_ARRAY_HEIGHT]) {
+void deal(DECK* deck) {
 	hand[0][1] = drawCard(deck);
 	hand[0][0] = drawCard(deck);
 	hand[1][1] = drawCard(deck);
 	hand[1][0] = drawCard(deck);
 }
 
-void draw(DECK* deck, CARD hand[HAND_ARRAY_LENGTH][HAND_ARRAY_HEIGHT]) {
+void draw(DECK* deck) {
 	CARD blank = createCard(0, 0);
 	for (int i = 0; i < HAND_ARRAY_LENGTH; i++) {
 		if (hand[i][1].value == blank.value) {
@@ -16,6 +16,6 @@ void draw(DECK* deck, CARD hand[HAND_ARRAY_LENGTH][HAND_ARRAY_HEIGHT]) {
 	}
 }
 
-void printHand(CARD hand[HAND_ARRAY_LENGTH][HAND_ARRAY_HEIGHT]) {
+void printHand() {
 
 }
