9a43e48ff4595c71ba47559b0793910334312960
SanctumSpear <146847788+SanctumSpear@users.noreply.github.com>
2025-03-19 13:29:45 -0400

    Cleanup

--- CSCN71030_Section2_Group2/main.c
+++ CSCN71030_Section2_Group2/main.c
@@ -8,18 +8,7 @@
 #include "Dealer.h"
 
 int main(void) {
-	//Test code for card and deck stuff
 
-	//CARD testCard = createCard(1, 1);
-	//printCard(testCard);
-	//printf_s("\n\n");
-
-	//DECK testDeck = createDeck();
-	//CARD deckCard = drawCard(&testDeck);
-	//printCard(deckCard);
-	//printf_s("\n\n");
-
-	//printDeck(testDeck);
 	char buffer[64] = "0";
 	char start[] = "Start";
 	do {
