40b2c4e7acd67e74a08846ee5261bb90f1708bac
Simonatoratorator <113464762+Simonatoratorator@users.noreply.github.com>
2025-03-20 10:11:25 -0400

    Put all c files into the unit tests source files

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -1,7 +1,17 @@
 #include "CppUnitTest.h"
+#include <stdbool.h>
+
+extern "C" {
+	#include "../CSCN71030_Section2_Group2/Bet.h"
+#include "../CSCN71030_Section2_Group2/Card.h"
+#include "../CSCN71030_Section2_Group2/Deck.h"
+#include "../CSCN71030_Section2_Group2/Dealer.h"
+#include "../CSCN71030_Section2_Group2/Hand.h"
+#include "../CSCN71030_Section2_Group2/Player.h"
+#include "../CSCN71030_Section2_Group2/ResolvingGame.h"
+}
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
 namespace UnitTests
 {
 	TEST_CLASS(PlayerTests) {
@@ -17,6 +27,14 @@ namespace UnitTests
 
 		TEST_METHOD(DealerTest_RememberToDoNamingConvention) {
 
+		}
+	};
+
+	TEST_CLASS(CardTests) {
+	public:
+
+		TEST_METHOD(CardTest_CreationTestValidCard) {
+		
 		}
 	};
 }
